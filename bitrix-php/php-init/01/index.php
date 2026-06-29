8f5cf2f7dc06bb2dc8290b10b7ba18b6e764ddee
Viktor Tishchenko <9043258451vvt@gmail.com>
2026-06-29 17:07:27 +0600

    restart php

---
+++ bitrix-php/php-init/01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document PHP</title>
+</head>
+<body>
+  <?php 
+      echo "Привет, мир!";
+  
+  ?>
+  
+</body>
+</html>
---
+++ bitrix-php/php-init/02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document PHP</title>
+</head>
+<body>
+
+<?php 
+// однострочный комментарий
+# однострочный комментарий
+/* это
+многострочный
+комментарий */
+echo "Halo, world!";
+?>
+<br>
+<?php print "Привет, мир ч/з print!"; ?>
+</body>
+</html>
